901fd495305a51c62fc47729fcf3c65a05e9ca72
EmVegito <ay2728850@gmail.com>
2025-09-22 16:22:41 +0530

    Deployment

---
+++ src/custom_exception.py
@@ -0,0 +1,19 @@
+class CustomException(Exception):
+
+    def __init__(self, error_message, error_detail):
+        super().__init__(error_message)
+        self.error_message = self.get_detailed_error_message(error_message, error_detail)
+
+    @staticmethod
+    def get_detailed_error_message(error_message, error_detail):
+        _, _, exc_tb = error_detail.exc_info()
+        
+        if exc_tb is not None:
+            file_name = exc_tb.tb_frame.f_code.co_filename
+            line_number = exc_tb.tb_lineno
+            return f"Error in {file_name}, line {line_number} : {error_message}"
+        else:
+            return f"Error: {error_message}"
+    
+    def __str__(self):
+        return self.error_message
